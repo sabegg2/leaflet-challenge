b255376b892b1488764262a3540efac6d3d085f4
sabegg2 <sabegg@gmail.com>
2024-09-20 07:50:42 -0600

    reworked code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,93 +1,71 @@
-// Earthquake geoJSON 
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Define earthquakes and tectonic plates GeoJSON url variables
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicplatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-// Function to create the map
-function createMap(eq){
+// Create two layerGroups
+var earthquakes = L.layerGroup();
+var tectonicplates = L.layerGroup();
 
-    // Create satellite tile
-    let sat = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
-        minZoom:2,
-        maxZoom: 12,
-        attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
-    });
+// Create satellite tile
+var satelliteMap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+  minZoom: 2,
+  maxZoom: 12,
+  attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+});
 
-    // Create topo Tile
-    let openTopo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        minZoom:2,
-        maxZoom: 12,
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
- 
-    // Create street Tile
-    let openStreet = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        minZoom:2,
-        maxZoom: 12,
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
+// Create topo tile
+var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  minZoom: 2,
+  maxZoom: 12,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
 
+// Create street tile
+var openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  minZoom: 2,
+  maxZoom: 12,
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
-    // baseMaps object
-    let baseMaps = {
-        "Satellite" : sat,
-        "OpenTopo": openTopo,
-        "OpenStreet": openStreet
-    };
+// Create baseMaps object
+var baseMaps = {
+  "Satellite" : satelliteMap,
+  "OpenTopo": openTopoMap,
+  "OpenStreet": openStreetMap
+};
 
-    // Create an overlay object
-    let overlayMaps = {
-        "Earthquakes" : eq
-    };
+// Create overlayMaps object to hold the overlay layers
+var overlayMaps = {
+  "Earthquakes": earthquakes,
+  "Tectonic Plates": tectonicplates
+};
 
-    // Create the map
-    let myMap = L.map("map",{
-        center: [0, 0],
-        zoom:2,
-        layers:[sat,eq]
-    });
+// Create the map, giving it the satelliteMap and earthquakes layers to display on load
+var myMap = L.map("map", {
+  center: [0, 0],
+  zoom: 2,
+  layers: [satelliteMap, earthquakes]
+});
 
-    // Create a layer control
-    var layerControl = L.control.layers(baseMaps, overlayMaps,{
-        collapsed:false
-    }).addTo(myMap);
+// Create a layer control
+// Pass in the baseMaps and overlayMaps
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
 
-    // Create the Tectonic Plate geoJSON
-    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then((geodata) => {
-
-    let plates = L.geoJson(geodata,{
-        style: {
-            color: "pink",            
-            fill: false
-        }
-    });
-
-    // Add the plates to the Overlay
-    layerControl.addOverlay(plates, "Tectonic Plates");
-
-    });
-
-    // Create a legend control
-    let legend = L.control({
-        position:"bottomright"
-    });
-
-    // Add a div class to legend control
-    legend.onAdd = function(){
-        let div = L.DomUtil.create("div", "legend");
-        return div;
-    };
-
-    // Add legend control to map
-    legend.addTo(myMap);
-
-}
-
-//createMap();
-
-// Function to create the earthquake markers
-function eqMarkers(response){
-
-    // Function to determine the marker color by depth
-    function markerColor(depth) {
+// Define functions for size and color of earthquake markers 
+d3.json(earthquakesURL, function(earthquakeData) {
+  // Determine the marker size by magnitude
+  function markerSize(magnitude) {
+    if (magnitude < 0.25) {
+      return 1;
+  } else {
+      return magnitude * 4;
+  }
+  };
+  // Determine the marker color by depth
+  function markerColor(depth) {
     switch(true) {
       case depth > 90:
         return "red";
@@ -104,61 +82,59 @@ function eqMarkers(response){
     }
   }
 
-    // Function to determine marker size
-    function markerSize(magnitude){
-        if (magnitude < 0.25) {
-            return 1;
-        } else {
-            return magnitude * 4;
+  // Create a GeoJSON layer containing the features array
+  // Each feature has popup describing the place/time/magnitude/depth of the earthquake
+  L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, 
+        // Set the style of the markers 
+        {
+          radius: markerSize(feature.properties.mag),
+          fillColor: markerColor(feature.geometry.coordinates[2]),
+          fillOpacity: 0.7,
+          color: "black",
+          stroke: true,
+          weight: 0.5
         }
+      );
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h4>Location: " + feature.properties.place 
+        + "</h4><hr>Date: " + new Date(feature.properties.time) 
+        + "<br>Magnitude: " + feature.properties.mag 
+        + "<br>Depth: " + feature.geometry.coordinates[2] );
     }
+  }).addTo(earthquakes);
+  // Send earthquakes layer to the map
+  earthquakes.addTo(myMap);
 
-    // Function to bindpopup
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h4>Earthquake Details</h4><hr/>\
-        <small><b>Date/Time:</b> ${new Date(feature.properties.time).toUTCString()}<br/>\
-        <b>Location:</b> ${feature.properties.place}<br/>\
-        <b>Lat:</b> ${feature.geometry.coordinates[1]}<br/>\
-        <b>Long:</b> ${feature.geometry.coordinates[0]}<br/>\
-        <b>Depth:</b> ${feature.geometry.coordinates[2]}<br/>\
-        <b>Magnitude:</b> ${feature.properties.mag}<br/>\
-        <a target="_blank" href=${feature.properties.url}>USGS Eventpage</a></small>`
-        )};
+  // Get the tectonic plate data from tectonicplatesURL
+  d3.json(tectonicplatesURL, function(data) {
+    L.geoJSON(data, {
+      color: "pink",
+      weight: 2
+    }).addTo(tectonicplates);
+    //tectonicplates.addTo(myMap); // uncomment if this layer is to be shown on load along with earthquakes
+  });
 
-    // Function to create marker
-    function createMarker(geoJsonPoint, latlng){
-        //console.log(geoJsonPoint);
-        return L.circleMarker(latlng, {
-            radius : markerSize(geoJsonPoint.properties.mag),
-            weight: 1,
-            color: "gray",
-            fillColor: markerColor(geoJsonPoint.geometry.coordinates[2]),
-            fillOpacity: 0.9
-        });
-    };
+  // Add legend
+  var legend = L.control({position: "bottomright"});
 
-    // Create the earthquake markers
-    let eq_markers = L.geoJson(response,{
-        pointToLayer : createMarker,
-        onEachFeature : onEachFeature
-        });
-    
-    // Call functions to create the map and legend
-    createMap(eq_markers);
-    createLegend();
-}
+  legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend"),
 
-// Function to create legend
-function createLegend(){
-    document.querySelector(".legend").innerHTML = [
-        "<h4>Depth Legend</h4><hr/>",
-        "<div class='legoption'><div class='box' style='background-color:lightgreen;'></div> -10-10</div>",
-        "<div class='legoption'><div class='box' style='background-color:yellow;'></div> 10-30</div>",
-        "<div class='legoption'><div class='box' style='background-color:gold;'></div> 30-50</div>",
-        "<div class='legoption'><div class='box' style='background-color:orange;'></div> 50-70</div>",
-        "<div class='legoption'><div class='box' style='background-color:orangered;'></div> 70-90</div>",
-        "<div class='legoption'><div class='box' style='background-color:red;'></div> 90+</div>",
-    ].join("");
-}
+      depths = [-10, 10, 30, 50, 70, 90];
+      
+      div.innerHTML = "<h4>Depth Legend (m)</h4><hr/>"
+  
+      for (var i=0; i < depths.length; i++) {
+        div.innerHTML += 
+        '<div class="box" style="background-color:'+ markerColor(depths[i] + 1) +'"></div> ' +
+            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');  // condition ? expressionIfTrue : expressionIfFalse
+        }
 
-d3.json(url).then(eqMarkers);
+      return div;
+  };
+      
+  legend.addTo(myMap);
+});
